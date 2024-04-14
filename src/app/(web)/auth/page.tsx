1b821dc1f86c7dbaf8fbe918d39a81141768c8df
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2024-04-14 21:22:41 +0100

    config for jwt token

--- src/app/(web)/auth/page.tsx
+++ src/app/(web)/auth/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { signUp } from "next-auth-sanity/client";
 import { signIn, useSession } from "next-auth/react";
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const Auth = () => {
   const [formData, setFormData] = useState({
@@ -20,16 +21,21 @@ const Auth = () => {
   };
 
   const { data: session } = useSession();
+  const router = useRouter();
 
-  console.log(session);
+  useEffect(() => {
+    if (session) router.push("/");
+  }, [router, session]);
 
-  const loginHandler = async () => {
-    try {
-      await signIn();
-    } catch (error) {
-      console.log(error);
-      toast.error("Something went wrong");
-    }
+  const loginHandler = () => {
+    signIn()
+      .then(() => {
+        router.push("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Something went wrong");
+      });
   };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
